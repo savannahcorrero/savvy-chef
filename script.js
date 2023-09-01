93fbc1ac6ca326d52dfba1840e4cdeb9b8d43a43
Savannah Correro <132963103+savannahcorrero@users.noreply.github.com>
2023-09-01 12:00:26 -0500

    Added JavaScript to search bar on index page

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // SEARCH BAR
+    let searchContent = "";
+    const box = document.querySelector(".auto-box");
+    
+    fetch("http://savvy-chef.savannahcorrero.com/search.json").then(async (response) => {
+        searchContent = await response.json();
+        searchContent.map((searchResult) => results(searchResult));
+    });
+
+    const results = (searchContent) => {
+        const { name, url } = searchContent;
+        const searchResult = document.createElement("li");
+        searchResult.className = "search-result";
+        searchResult.innerHTML = `<a href="${url}" target="_blank">${name}</a>`;
+        box.append(searchResult);
+
+        if (searchResult.length == 0) {
+            searchResult.innerHTML = "No results found"
+        };
+    };
+
+    const handleResultPosts = (query) => {
+        const searchQuery = query.trim().toLowerCase();
+        
+        if (searchQuery.length <= 1) {
+          return
+        }
+    };
+
+    let resultFilter = [...searchContent].filter(
+        (searchResult) =>
+        searchResult.categories.some((category) => category.toLowerCase().includes(searchQuery)) ||
+        searchResult.title.toLowerCase().includes(query)
+    );
+
+    box.innerHTML = "";
+    resultFilter.map((searchResult) => results(searchResult));
+
+    const reset = () => {
+        search.innerHTML = ""
+        searchResult.innerHTML = "";
+        searchResult.map((searchResult) => results(searchResult));
+    };
+    
+    const search = document.querySelector(".search-bar");
+    let debounceTimer;
+    
+    const debounce = (callback, time) => {
+        window.clearTimeout(debounceTimer);
+        debounceTimer = window.setTimeout(callback, time);
+    };
+
+    search.addEventListener(
+        "input",
+        (event) => { 
+            const query = event.target.value;
+            debounce(() => handleResults(query), 500);
+        },
+        false
+    );
+
+
+    // PHOTO CAROUSEL
     let slideIndex = 1;
     let t;
     let btns = document.querySelectorAll(".nav-btn");
